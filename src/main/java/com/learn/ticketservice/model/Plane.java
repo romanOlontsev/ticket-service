4345d18246b61a60444f6d2955e3897566a4d986
romanOlontsev <roverviner@yandex.ru>
2022-03-10 16:08:45 +0300

    some changes

--- src/main/java/com/learn/ticketservice/model/Plane.java
+++ src/main/java/com/learn/ticketservice/model/Plane.java
@@ -4,10 +4,20 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.time.Duration;
+import java.time.LocalDate;
 
 @Entity
 @Data
 public class Plane {
     @Id
     private Long id;
+    private String name;
+    private Integer places;
+    private LocalDate dapart;
+    private Duration duration;
+    private String from;
+    private String to;
+//    private List<Ticket> tickets;
+    private boolean isDeleted;
 }
--- src/main/java/com/learn/ticketservice/service/dto/PlaneDto.java
+++ src/main/java/com/learn/ticketservice/service/dto/PlaneDto.java
@@ -3,8 +3,19 @@ package com.learn.ticketservice.service.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.Duration;
+import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 public class PlaneDto {
     private long id;
+    private String name;
+    private Integer places;
+    private LocalDate dapart;
+    private Duration duration;
+    private String from;
+    private String to;
+    //    private List<Ticket> tickets;
+    private boolean isDeleted;
 }
